3f1a588499b8499aa175ab195d3fdb59924aef72
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2026-08-30 22:06:22 +0300

    fix: generate cv locally and bypass vercel puppeteer error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@
 import { defineConfig } from 'astro/config';
 import pdf from 'astro-pdf';
 
+// Check if code is executed at Vercel cloud
+const isVercel = !!process.env.VERCEL;
+
 // https://astro.build/config
 export default defineConfig({
   server: {
@@ -11,18 +14,29 @@ export default defineConfig({
     chromeDevtoolsWorkspace: true,
     contentIntellisense: true,
   },
-  integrations: [
-    pdf({
-      pages: {
-        '/': 'Tomi_Sarjamo_CV.pdf',
-      },
-      baseOptions: {
-        pdf: {
-          format: 'A4',
-          printBackground: true,
-          margin: { top: '20mm', bottom: '20mm', left: '20mm', right: '20mm' },
-        },
-      },
-    }),
-  ],
+  // Activate integration only in when Vercel is not used
+  integrations: isVercel
+    ? []
+    : [
+        pdf({
+          server: {
+            port: 4000,
+          },
+          pages: {
+            '/': 'Tomi_Sarjamo_CV.pdf',
+          },
+          baseOptions: {
+            pdf: {
+              format: 'A4',
+              printBackground: true,
+              margin: {
+                top: '20mm',
+                bottom: '20mm',
+                left: '20mm',
+                right: '20mm',
+              },
+            },
+          },
+        }),
+      ],
 });
